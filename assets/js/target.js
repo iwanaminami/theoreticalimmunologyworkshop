ced6aaca2a343aa4a0bc34a5e5cfdab100361d4e
Iwanami <iwanamishoya@Iwanami-iMac-Pro.local>
2021-01-21 20:49:36 +0900

    udpate

--- assets/js/target.js
+++ assets/js/target.js
@@ -2,6 +2,7 @@
 $(document).ready( function () {
     var domain = location.href.match(/^http?(s)?(:\/\/[a-zA-Z0-9-.:]+)/i)[0];
     $('a[href^=http]').not('[href*="'+domain+'"]').attr('target','_blank');
+    $('a[href^=http]').not('[href*="'+domain+'"]').attr('rel','noopener noreferrer');
 
     // ドメイン名の取得はlocationプロパティでも取得することができます。
     // 正規表現で取得した例
